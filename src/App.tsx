ef06d93e042b21a82eddae845de59a341278525a
bflorestal <yayou7710@hotmail.fr>
2023-04-14 10:49:05 +0200

    Component SearchBar

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 // Import de la liste de produits et du component ProductList
 import { PRODUCTS as productList } from "./productList";
 import ProductList from "./components/ProductList";
+import SearchBar from "./components/SearchBar";
 
 export default function App() {
   // On utilise useState pour stocker la valeur de l'input de recherche
@@ -16,15 +17,7 @@ export default function App() {
     <div className="App max-w-7xl w-full mx-auto">
       <div className="flex flex-col items-center justify-center py-6 gap-4">
         <h1 className="text-3xl font-bold">Liste de produits</h1>
-        <input
-          type="text"
-          name="product_name"
-          id="product_name"
-          placeholder="Rechercher..."
-          value={searchTerm}
-          onChange={handleChange}
-          className="form-input px-4 py-3 rounded-2xl text-black"
-        />
+        <SearchBar handleChange={handleChange} searchTerm={searchTerm} />
       </div>
       {/* On passe la liste de produits et la valeur de l'input en props */}
       <ProductList data={productList} searchTerm={searchTerm} />

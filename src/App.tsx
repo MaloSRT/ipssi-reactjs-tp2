7e2d1b83995d3c8951fc6736bf603fcdb70bf271
bflorestal <yayou7710@hotmail.fr>
2023-04-14 10:05:09 +0200

    Ajout liste de produits

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,45 @@
 import { useState } from "react";
+import { PRODUCTS } from "./productList";
 import "./App.css";
+import { Product } from "./types/product";
+
+type ProductListProps = {
+  data: Product[];
+  searchTerm: string;
+};
+
+function ProductList({ data, searchTerm }: ProductListProps) {
+  // On filtre les produits en fonction de la valeur de searchTerm
+  const filteredProducts = data.filter((product) => {
+    // On garde les produits dont le nom ou la catégorie contient la valeur de searchTerm
+    const filteredList =
+      product.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      product.category.toLowerCase().includes(searchTerm.toLowerCase());
+
+    return filteredList;
+  });
+
+  return (
+    <table style={{ width: "100%" }}>
+      <thead>
+        <tr>
+          <th>Catégorie</th>
+          <th>Nom</th>
+          <th>Prix</th>
+        </tr>
+      </thead>
+      <tbody>
+        {filteredProducts.map((product, index) => (
+          <tr key={index}>
+            <td>{product.category}</td>
+            <td>{product.name}</td>
+            <td>{product.price}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
 
 export default function App() {
   // On utilise useState pour stocker la valeur de l'input de recherche
@@ -22,6 +62,7 @@ export default function App() {
         onChange={handleChange}
       />
       <br />
+      <ProductList data={PRODUCTS} searchTerm={searchTerm} />
     </div>
   );
 }

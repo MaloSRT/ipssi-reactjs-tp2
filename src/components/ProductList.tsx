1385b2742a3275922d2b6cc92513875505c8b81d
bflorestal <yayou7710@hotmail.fr>
2023-04-14 10:09:53 +0200

    Séparation en components

---
+++ src/components/ProductList.tsx
@@ -0,0 +1,40 @@
+import { Product } from "../types/product";
+
+type ProductListProps = {
+  data: Product[];
+  searchTerm: string;
+};
+
+// On récupère les produits et la valeur de l'input en props
+export default function ProductList({ data, searchTerm }: ProductListProps) {
+  // On filtre les produits en fonction de la valeur de searchTerm
+  const filteredProducts = data.filter((product) => {
+    // On garde les produits dont le nom ou la catégorie contient la valeur de searchTerm
+    const filteredList =
+      product.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      product.category.toLowerCase().includes(searchTerm.toLowerCase());
+
+    return filteredList;
+  });
+
+  return (
+    <table style={{ width: "100%" }}>
+      <thead>
+        <tr>
+          <th>Catégorie</th>
+          <th>Nom</th>
+          <th>Prix</th>
+        </tr>
+      </thead>
+      <tbody>
+        {filteredProducts.map((product, index) => (
+          <tr key={index}>
+            <td>{product.category}</td>
+            <td>{product.name}</td>
+            <td>{product.price}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}

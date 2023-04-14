fe41e987d1b8195c916b87a116e435a6884392b2
bflorestal <yayou7710@hotmail.fr>
2023-04-14 09:57:34 +0200

    Ajout barre de recherche

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,27 @@
+import { useState } from "react";
 import "./App.css";
 
 export default function App() {
+  // On utilise useState pour stocker la valeur de l'input de recherche
+  const [searchTerm, setSearchTerm] = useState("");
+
+  // La fonction handleChange permet de mettre à jour dynamiquement la valeur de searchTerm
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(e.target.value);
+  };
+
   return (
     <div className="App">
-      <h1>Hello world!</h1>
+      <h1>Liste de produits</h1>
+      <input
+        type="text"
+        name="product_name"
+        id="product_name"
+        placeholder="Rechercher..."
+        value={searchTerm}
+        onChange={handleChange}
+      />
+      <br />
     </div>
   );
 }
